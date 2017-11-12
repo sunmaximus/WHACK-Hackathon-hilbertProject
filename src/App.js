96c56e8c77a5279fb5facd3d50e9c9879bd3edfa
Son Nguyen <sonnguyen@Sons-MacBook-Pro.local>
2017-11-11 23:35:33 -0500

    Fixed Event listener on Mouse and to get the right frequencies.

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { arrayOfPixels: [], imageHeight: 0, imageWidth: 0, rgba: 'rgba(0, 0, 0, 0)' };
+    this.state = { arrayOfPixels: [], imageHeight: 300, imageWidth: 300, rgba: 'rgba(0, 0, 0, 0)' };
   }
   
   componentWillMount() {
@@ -40,14 +40,13 @@ class App extends Component {
   }
 
   componentDidMount() {
+    const self = this;    
     const ctx = this.refs.canvas.getContext('2d');
     // important need or else it won't work. 
     img.onload = () => {
       ctx.drawImage(img, 0, 0);
       img.style.display = 'none';
     }
-
-    const self = this;
 
     const pick = (event) => {
       let x = event.layerX;
@@ -65,100 +64,112 @@ class App extends Component {
 
     }
     // this.refs.canvas.addEventListener('mousemove', (event) => pick(event));
-      //global variable soundOn, context, o 
-      // var soundOn = false
-      var contexts = []
-      var oscillators = []
-     // var gains = []
-     /* var pianoGains = [1.0, 0.399064778, 0.229404484, 0.151836061,
-                      0.196754229, 0.093742264, 0.060871957,
-                      0.138605419, 0.010535002, 0.071021868,
-                      0.029954614, 0.051299684, 0.055948288,
-                      0.066208224, 0.010067391, 0.00753679,
-                      0.008196947, 0.012955577, 0.007316738,
-                      0.006216476, 0.005116215, 0.006243983,
-                      0.002860679, 0.002558108, 0.0, 0.001650392]*/
+    //global variable soundOn, context, o 
+    var soundOn = false
+    var contexts = []
+    var oscillators = []
+    // var gains = []
+    /* var pianoGains = [1.0, 0.399064778, 0.229404484, 0.151836061,
+                    0.196754229, 0.093742264, 0.060871957,
+                    0.138605419, 0.010535002, 0.071021868,
+                    0.029954614, 0.051299684, 0.055948288,
+                    0.066208224, 0.010067391, 0.00753679,
+                    0.008196947, 0.012955577, 0.007316738,
+                    0.006216476, 0.005116215, 0.006243983,
+                    0.002860679, 0.002558108, 0.0, 0.001650392]*/
       
-      for (let i = 0; i < 3; i++){
-          contexts[i] = new AudioContext()
-      }
-      for (let i = 0; i < 3; i++){
-          oscillators[i] = contexts[i].createOscillator('sine')
-          oscillators[i].connect(contexts[i].destination)
-          oscillators[i].start()
-          contexts[i].suspend()
-          //gains[i] = contexts[i].createGain()
-          //gains[i].connect(contexts[i].destination);
-      }
-      //o.type = "sine"
-      
-      //returns a frequency between 200 and 800 Hz, and takes an rgba tuple/list/array
-      const scaleToFrequency = (rgba) => {
-          var multipliedTotal = rgba[0] * rgba[1] * rgba[2]
-          return (multipliedTotal/(255*255*255))*600 + 200 
-      }
-      
-      //takes a frequency and plays a sound, returns the AudioContext() object so that it can be stopped later
-      const playSound = (frequency) => {
-          /*for (i = 0; i < 6; i++){
-              oscillators[i].frequency.value = frequency*(i+1)
-              gains[i].gain.value = pianoGains[i]
-              oscillators[i].start()
-          }*/
-          oscillators[0].frequency.value = frequency
-          oscillators[1].frequency.value = frequency*5/4
-          oscillators[2].frequency.value = frequency*3/2
+    for (let i = 0; i < 3; i++){
+        contexts[i] = new AudioContext()
+    }
+    for (let i = 0; i < 3; i++){
+        oscillators[i] = contexts[i].createOscillator('sine')
+        oscillators[i].connect(contexts[i].destination)
+        oscillators[i].start()
+        contexts[i].suspend()
+        //gains[i] = contexts[i].createGain()
+        //gains[i].connect(contexts[i].destination);
+    }
+    //o.type = "sine"
+    
+    //returns a frequency between 200 and 800 Hz, and takes an rgba tuple/list/array
+    const scaleToFrequency = (rgba) => {
+        var multipliedTotal = rgba[0] * rgba[1] * rgba[2]
+        return (multipliedTotal/(255*255*255))*600 + 200 
+    }
+    
+    //takes a frequency and plays a sound, returns the AudioContext() object so that it can be stopped later
+    const playSound = (frequency) => {
+        /*for (i = 0; i < 6; i++){
+            oscillators[i].frequency.value = frequency*(i+1)
+            gains[i].gain.value = pianoGains[i]
+            oscillators[i].start()
+        }*/
+        oscillators[0].frequency.value = frequency
+        oscillators[1].frequency.value = frequency*5/4
+        oscillators[2].frequency.value = frequency*3/2
 
-          contexts[0].resume()
-          contexts[1].resume()
-          contexts[2].resume()
+        contexts[0].resume()
+        contexts[1].resume()
+        contexts[2].resume()
       }
       
       const stopSound = () => {
         for (let i = 0; i < 3; i++){
             contexts[i].suspend()
 
-//            oscillators[i] = contexts[i].createOscillator('sine')
-//            oscillators[i].connect(contexts[i].destination)
-            
+    //            oscillators[i] = contexts[i].createOscillator('sine')
+    //            oscillators[i].connect(contexts[i].destination)
         }
     }
-      
-      const mouseDown = (evt) => {
-          //toggle soundOn 
-          // soundOn = (soundOn) ? false : true;
-
-          console.log('figuring out what is wrong', evt)
-          if (evt.type === 'mouseout') return stopSound();
-
-          return mouseMove(evt)
-          console.log("In Mouse Down")
+    
+    const mouseDown = (evt) => {
+      //toggle soundOn 
+      soundOn = (soundOn) ? false : true;
+      if (soundOn){
+          mouseMove(evt)
       }
-      
-      
-      const mouseMove = (evt) => {
-          //pick(evt) returns an rgba tuple, scale changes it to a frequency, and play plays that frequency
-          playSound(scaleToFrequency(pick(evt)))
-          console.log("In Mouse Move")
-          //compose(play(), scale(), pick(evt))
-          if (self.state.isMajor){
-            playSound(scaleToFrequency(pick(evt)), 5/4)
-            console.log('isMajor')
-          }
-          else{
-            console.log('isMinnor')
-            playSound(scaleToFrequency(pick(evt)), 6/5)
-          }
+      else{
+          stopSound()
       }
+      console.log("In Mouse Down")
       
-      //event handlers
-      this.refs.canvas.addEventListener('mouseout', (event) => mouseDown(event))
-      this.refs.canvas.addEventListener('mouseover', (event) => mouseMove(event))
-  }0
+        // if (evt.type === 'mouseout') return stopSound();
+        // return mouseMove(evt)
+        // console.log("In Mouse Down")
+    }
+    
+    const mouseMove = (evt) => {
+        //pick(evt) returns an rgba tuple, scale changes it to a frequency, and play plays that frequency
+        playSound(scaleToFrequency(pick(evt)))
+        console.log("In Mouse Move")
+        //compose(play(), scale(), pick(evt))
+        if (self.state.isMajor){
+          playSound(scaleToFrequency(pick(evt)), 5/4)
+          console.log('isMajor')
+        }
+        else{
+          console.log('isMinnor')
+          playSound(scaleToFrequency(pick(evt)), 6/5)
+        }
+    }
+    
+    const mouseOut = (event) => {
+      if (event.type === 'mouseout') return stopSound();
+      return mouseMove(event)
+      console.log("In Mouse Down")
+    }
+
+    //event handlers
+    this.refs.canvas.addEventListener('mousedown', (event) => mouseDown(event)) // Click on picture
+    this.refs.canvas.addEventListener('mouseout', (event) => mouseOut(event)) // Not in picutre
+    this.refs.canvas.addEventListener('mousemove', (event) => mouseMove(event)) // Moving in picture
+  }
 
   render() {
     // console.log(this.state.imageHeight, this.state.imageWidth)
     const { imageWidth, imageHeight, rgba, isMajor, isMinnor } = this.state;
+
+    console.log(imageWidth, imageHeight)
     return (
       <div>
         <div className='custom-picture-container'>
@@ -166,7 +177,7 @@ class App extends Component {
             <canvas ref="canvas" width={imageWidth === 0 ? `300` : imageWidth} height={imageHeight === 0 ? `300` : imageHeight}/>
           </div>
           <div className='right' >
-            <div style={{ width: imageWidth === 0 ? `${300/2}px` : imageWidth, height: imageHeight === 0 ? `${300/2}px` : imageHeight/2, backgroundColor: rgba }}>
+            <div style={{ width: imageWidth === 0 ? `${300}px` : imageWidth, height: imageHeight === 0 ? `${300/2}px` : imageHeight/2, backgroundColor: rgba }}>
               {this.state.rgba}
             </div>
             <Checkbox checked={isMajor} onClick={() => this.setState({ isMajor: (isMajor ? false : true)})} label='Major' type='radio' />
